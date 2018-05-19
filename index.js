71cce1bbf2e1ab3f25092d20b9e661c7d09a64a0
Samantha brake <sammy.brake@gmail.com>
2018-05-19 00:01:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,15 @@ return rankedLists;
 }
 
 function deepestChild() {
+<<<<<<< HEAD
   let grandNode = document.querySelector('#grand-node div div div div');
   return grandNode;
 
+=======
+  let grandNode = document.getElementById('#grand-node div div div div')
+  return grandNode
+ 
+>>>>>>> 99a796b9eeb517d0b2aac817d1dcc0dd4f54cb87
   
   
   
